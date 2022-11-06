2c8b8e4d2f145994849e3a97db94fd677959988e
Sara R Prescott <sararprescott@yahoo.com>
2022-11-06 12:37:16 -0500

    Update Gettin There

--- contactform.php
+++ contactform.php
@@ -2,19 +2,19 @@
 
 if(isset($_POST['submit'])) {
     $name = $_POST['name'];
-    $city = $_POST['city'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
+    $subject = $_POST['subject'];
     $mailFrom = $_POST['mail'];
     $message = $_POST['message'];
+    $city = $_POST['city'];
+    $phone = $_POST['phone'];
     
-    $mailTo = "info@replacemysink.com" "sara@terraconinstall.com";
-    $headers = "Website Contact From: ".$mailFrom;
-    $txt = "You have received an email from ".$name.".\n\n".$name .$city .$email .$phone .$message;
+    $mailTo = "sara@terraconinstall.com";
+    $headers = "Email: ".$mailFrom;
+    $txt = "Name: ".$subject."\n\n City: ".$city."\n\n Phone: ".$phone."\n\n Email: ".$mailFrom."\n\n Message: ".$message;
     
     mail($mailTo, $subject, $txt, $headers);
     
-    header("Location: index.php?mailsend");
+    header("Location: index.php#about");
 }
 
 ?>
